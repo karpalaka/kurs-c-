d32a1436a7fc99c4f4b3175d2c2b890bef19c631
karpalaka <doge0468@gmail.com>
2025-12-06 12:55:26 +0100

    Update zadanie1.cpp

--- zadanie1.cpp
+++ zadanie1.cpp
@@ -1,1 +1,38 @@
+#include<iostream>
+#include<string>
 
+using namespace std;
+
+int main()
+{
+    int pierwsza_liczba, druga_liczba, trzecia_liczba;
+
+    cout << "Podaj pierwsza liczbe:";
+    cin >> pierwsza_liczba;
+    cout << "Podaj druga liczbe:";
+    cin >> druga_liczba;
+    cout << "Podaj trzecią liczbe:";
+    cin >> trzecia_liczba;
+    cout << "\nSrednia: " << pierwsza_liczba + druga_liczba + trzecia_liczba/3;
+    int promien;
+    cout << "\nPodaj pierwsza liczbe";
+    cin >> promien;
+    cout << "\nPole kola" << 3.14 * (promien * promien);
+    int stopnie;
+    cout << "\nPodaj stopnie celcjusza";
+    cin >> stopnie;
+    cout << "\nFahrenhait:" << stopnie * 1.8 + 32;
+    int cena_paliwo;
+    cout << "\nPodaj cene paliwa";
+    cin >> cena_paliwo;
+    cout << "\nKoszt Paliwa" << 0.09 * 335 * cena_paliwo;
+    int dlugosc_trasy, spalanie_auta;
+    cout << "\nPodaj dlugosc trasy";
+    cin >> dlugosc_trasy;
+    cout << "\nPodaj spalanie (L/100km)";
+    cin >> spalanie_auta;
+    cout << "\nKoszt Paliwa na tej trasie" << (spalanie_auta / 100) * dlugosc_trasy * cena_paliwo;
+    
+    return 0;
+
+}
